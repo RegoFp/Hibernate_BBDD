15d15957a528445ae425a6a973bdeaccc907c9fd
RegoFp <92778484+RegoFp@users.noreply.github.com>
2022-01-26 13:09:00 +0100

    Initial commit
    dept es un placeholder

---
+++ src/main/java/com/mycompany/hibernate_bbdd/dept.java
@@ -0,0 +1,35 @@
+package com.mycompany.hibernate_bbdd;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.awt.color.ICC_ColorSpace;
+
+/**
+ *
+ * @author Developer
+ */
+
+public class dept {
+    
+    private int deptno;
+    private String dname;
+    private String loc;
+    
+    
+    
+    public dept(int _deptno, String _dname, String _loc){
+        deptno = _deptno;
+        dname=_dname;
+        loc=_loc;
+    }
+        
+    
+    
+}
+
+
