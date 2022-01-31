31ecb9950cfba344c445defcf18e2c2405e534c3
Rocío <92778814+rocioseoane@users.noreply.github.com>
2022-01-31 16:45:50 +0100

    Creación de getters y setters.

--- src/main/java/com/mycompany/hibernate_bbdd/emp.java
+++ src/main/java/com/mycompany/hibernate_bbdd/emp.java
@@ -38,8 +38,57 @@ public class emp {
         this.comm=comm;
         this.deptno=deptno;
     }
-
+    
+    public int getEmpno(){
+        return empno;
     }
+    public String getEname(){
+        return ename;
+    }
+    public String getJob(){
+        return job;
+    }
+    public int getMgr(){
+        return mgr;
+    }
+    public Date getHiredate(){
+        return hiredate;
+    }
+    public float getSal(){
+        return sal;
+    }
+    public float getComm(){
+        return comm;
+    }
+    public float getDeptno(){
+        return deptno;
+    }
+    
+    public void setEmpno(int _empno){
+        this.empno=_empno;
+    }
+    public void setEname(String _ename){
+        this.ename=_ename;
+    }
+    public void setJob(String _job){
+        this.job=_job;
+    }
+    public void setMgr(int _mgr){
+        this.mgr=_mgr;
+    }
+    public void setHiredate(Date _hiredate){
+        this.hiredate=_hiredate;
+    }
+    public void setSal(float _sal){
+        this.sal=_sal;
+    }
+    public void setComm(float _comm){
+        this.comm=_comm;
+    }
+    public void setDeptno(float _deptno){
+        this.deptno=_deptno;
+    }
+}
 
 
 
