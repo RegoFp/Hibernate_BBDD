6d81c271e4bbca77aa4a7af8ddf6c7c0d82ef1bc
RegoFp <92778484+RegoFp@users.noreply.github.com>
2022-02-01 11:47:51 +0100

    hibernateUtil actualizado

--- src/main/java/com/mycompany/hibernate_bbdd/HibernateUtil.java
+++ src/main/java/com/mycompany/hibernate_bbdd/HibernateUtil.java
@@ -1,9 +1,12 @@
 package com.mycompany.hibernate_bbdd;
 
+import java.util.Properties;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
@@ -11,12 +14,32 @@ public class HibernateUtil {
   private static SessionFactory sessionFactory;
   private static Session session;
 	
+
+  
   /**
+   *<property name="hibernate.connection.driver_class"></property>
+    <property name="hibernate.connection.url">jdbc:mysql://localhost/basededatos</property>
+    <property name="hibernate.connection.username">usuario</property>
+    <property name="hibernate.connection.password">contraseña</property>
+    <property name="hibernate.dialect">org.hibernate.dialect.MySQL5Dialect</property>
+    <property name="hibernate.show_sql">true</property>
+   * 
    * Crea la factoria de sesiones
    */
   public static void buildSessionFactory() {
 
+    Properties properties = new Properties();
+    properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+    properties.put(Environment.URL, "database-1.cvliipm1ojas.eu-west-2.rds.amazonaws.com");
+    properties.put(Environment.USER, "admin");
+    properties.put(Environment.PASS, "administrador");
+    properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+    properties.put(Environment.SHOW_SQL, true);
+    
+
+
     Configuration configuration = new Configuration();
+    configuration.setProperties(properties);
     configuration.configure();
     // Se registran las clases que hay que mapear con cada tabla de la base de datos
     configuration.addAnnotatedClass(dept.class);
@@ -38,6 +61,7 @@ public class HibernateUtil {
    * Devuelve la sesión actual
    * @return
    */
+  
   public static Session getCurrentSession() {
 	
     if ((session == null) || (!session.isOpen()))
